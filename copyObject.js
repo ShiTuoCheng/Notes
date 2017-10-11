e53da7cb37187c214bfc852bb06edd5f2bb04977
史拓成 <shituocheng@126.com>
2017-10-11 06:22:20 -0500

    Create copyObject.js

---
+++ copyObject.js
@@ -0,0 +1,67 @@
+/*jshint esversion:6*/
+
+//js浅复制函数
+const shallowCopy = (obj) => {
+
+	if (typeof obj !== "object"){
+
+		return;
+	}
+	
+	let newObj;
+
+	if (Object.prototype.toString.call(obj) === "[Object Array]") {
+
+		newObj = [];
+
+	}else{
+
+		newObj = {};
+		newObj.constructor = obj.constructor;
+	}
+
+	for(let eachProp in obj){
+
+		if (obj.hasOwnProperty(eachProp)) {
+
+			newObj[eachProp] = obj[eachProp];
+		}
+	}
+
+	return newObj;
+};
+
+//js 深复制函数
+const deepCopy = (obj) => {
+
+	if(typeof obj !== "object"){
+
+		return;
+	}
+
+	let newObj;
+
+	if(Object.prototype.toString.call(obj) === "[Object Array]"){
+
+		newObj = [];
+	}else{
+
+		newObj = {};
+		newObj.constructor = obj.constructor;
+	}
+
+	for(let key in obj){
+		if(obj.hasOwnProperty(key)){
+
+			//递归实现深复制（判断是否为对象若为对象则递归实现深复制，若为基础类型则直接浅复制）
+			if(typeof obj[key] === obj){
+
+				newObj[key] = deepCopy(obj[key]);
+			}else{
+
+				newObj[key] = obj[key];
+			}
+		}
+	}
+	
+};
